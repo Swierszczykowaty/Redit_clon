93ed5c672fcc1b1755348aad1db226cde5737c15
Swierszczykowaty <loolok1232@gmail.com>
2024-02-06 12:59:10 +0100

    [ADD] - treniding component

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,65 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { useState } from 'react';
+import './App.css';
 import { PiImageSquareLight } from "react-icons/pi";
 import { GoLink } from "react-icons/go";
+import { BsRocket } from "react-icons/bs";
+import { AiOutlineFire } from "react-icons/ai";
+import { FaRegSun } from "react-icons/fa6";
+import { IoPodiumOutline } from "react-icons/io5";
+import { BsThreeDots } from "react-icons/bs";
+import { RxViewHorizontal } from "react-icons/rx";
+import { RiArrowDownSLine } from "react-icons/ri";
+import Post from './Post';
 
+
+function Button_small({ text, icon, className  }) {
+  return (
+    <div >
+      <button className={`button_main ${className}`}>
+        <span className='b_s_image'>{icon}</span>
+        <span className='b_s_text'>{text}</span>
+      </button>
+    </div>
+  );
+}
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div className="main">
-        <div className='create'>
+        <div className='create' id='search'>
           <div className='avatar_circle'>
             <img src="./imagines/344308096_547741020772353_384304532245554270_n.jpg" alt="" className='avatar'/>
           </div>
-          
-            <input type="text" placeholder='Create Post' className='search' />
+          <input type="text" placeholder='Create Post' className='search' />
           <div className='icons'>
-            <PiImageSquareLight className='imagine'/>
-            <GoLink className='imagine'/>
+            <button className='imagine_button'>
+             <PiImageSquareLight className='imagine'/>
+            </button >
+             <button className='imagine_button'>
+              <GoLink className='imagine'/>
+            </button>
           </div>
         </div>
-        <div className='create'>
-              s
+        <div className='create' id='trend'>
+          <Button_small text="Best" icon={<BsRocket size={25}/>} />
+          <Button_small text="Hot" icon={<AiOutlineFire size={25}/>} />
+          <Button_small text="New" icon={<FaRegSun size={25}/>} />
+          <Button_small text="Top" icon={<IoPodiumOutline size={25}/>} />
+          <button className='dotdotdot'>
+            <BsThreeDots className='dot'/>
+          </button>
+          <div className='view_dot'>
+          <RxViewHorizontal className='view'/>
+          <RiArrowDownSLine className='view'/>
+          </div>
         </div>
-        
+        <Post/>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Post.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useState } from 'react';
+import './App.css';
+
+
+
+function Post() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+     <div className='post'>
+      
+     </div>
+    </>
+  );
+}
+
+export default Post;
