4365df4dd37648a511a5395daeaea16ab65b2512
Swierszczykowaty <loolok1232@gmail.com>
2024-02-07 19:17:02 +0100

    [ADD] - end Post, end I column

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import { RiArrowDownSLine } from "react-icons/ri";
 import Post from './Post';
 
 
-function Button_small({ text, icon, className  }) {
+function Button_small({ text, icon, className}) {
   return (
     <div >
       <button className={`button_main ${className}`}>
@@ -28,35 +28,92 @@ function App() {
 
   return (
     <>
-      <div className="main">
-        <div className='create' id='search'>
-          <div className='avatar_circle'>
-            <img src="./imagines/Iganc Openhaimer.png" alt="" className='avatar'/>
+      <div className='main'>
+        <div className="first_column">
+          <div className='create' id='search'>
+            <div className='avatar_circle'>
+              <img src="./imagines/Iganc Openhaimer.png" alt="" className='avatar'/>
+            </div>
+            <input type="text" placeholder='Create Post' className='search' />
+            <div className='icons'>
+              <button className='imagine_button'>
+              <PiImageSquareLight className='imagine'/>
+              </button >
+              <button className='imagine_button'>
+                <GoLink className='imagine' id='link'/>
+              </button>
+            </div>
           </div>
-          <input type="text" placeholder='Create Post' className='search' />
-          <div className='icons'>
-            <button className='imagine_button'>
-             <PiImageSquareLight className='imagine'/>
-            </button >
-             <button className='imagine_button'>
-              <GoLink className='imagine'/>
+          <div className='create' id='trend'>
+            <Button_small text="Best" icon={<BsRocket size={25}/>} />
+            <Button_small text="Hot" icon={<AiOutlineFire size={25}/>} />
+            <Button_small text="New" icon={<FaRegSun size={25}/>} />
+            <Button_small text="Top" icon={<IoPodiumOutline size={25}/>} />
+            <button className='dotdotdot'>
+              <BsThreeDots className='dot'/>
             </button>
+            <div className='view_dot'>
+            <RxViewHorizontal className='view'/>
+            <RiArrowDownSLine className='view'/>
+            </div>
           </div>
+          <Post 
+            avatar_sub="./imagines/uwaga.png"
+            subredditName="r/Cykadło"
+            user="u/Rocha2115"
+            time="4"
+            title="How do you get enough protein in your diet to promote muscle growth from your workouts?"
+            mark="Workout"
+            markColor="#8d6a9f"
+            post={
+              <div>
+                <p>I just started tracking macros and... it's depressing. I thought I was doing ok but I'm having way too little protein.</p>
+                <p>I don't really like meat other than red meat, like I can't just sit and eat a piece of chicken. But yesterday I had (among other things): peanut butter on sprouted bread that had protein, cottage cheese, a protein shake, collagen supplement, and chicken sausage for dinner as my main sources of protein and I was way way under the recommended 120 grams. I was at like 78 g. I also had Hummus and an avocado, which I know have a little protein too.</p>
+                <p>I have been doing more bodyweight exercises, but I'm not seeing the results I want and I think it's diet related. What do you all do here to get enough protein to supplement workouts/what would you recommend for me?</p>
+                <p>Edit: I can't reply to everyone, so I'll reply here: thank you all so much!! I have SO many ideas to go off of now. Info I should have included in the post: I'm a woman, and all of my food related goals in the past have been stay skinny so it's been all about cutting calories. This is my first time eating with the goal of being stronger, instead of it being appearance based. I'm so excited to try all of these foods and continue to build strength!</p>
+              </div>
+            }
+          />
+          <Post 
+            avatar_sub="./imagines/WWSI.ico"
+            subredditName="r/Wwsi"
+            user="u/Meferek"
+            time="7"
+            title="How to create a group project?"
+            mark="Projekt"
+            markColor="#ff4500"
+            post={
+              <div>
+                <p>Hi Everybody,</p>
+                <p>I'm making this post because initially when I was a beginner learning to code, I really struggled to make projects, I couldn't quite get out of tutorial hell.</p>
+                <p>Initially to overcome this, I posted my frustrations here. The responses I received were all centered around building my own projects. This advice sounds great on the surface and was the right advice, yet the issue for me was I couldn't even watch a tutorial and reproduce the results. How was I supposed to build a project? A project is 10 levels above a tutorial and at the time it seemed that projects were so far away from my abilities. I had no clue how to even get started, let alone how I was going to put it all together.</p>
+                <p>This is an issue I see all the time on r/learnprogramming. Beginners are frustrated that they can't seem to get out of tutorial hell and so they seek advice. But, what they get is well meaning redditor's slamming the build projects response in their face without even considering that if the newbie can't even replicate a tutorial how are they going to create their own project. I thought it was about time I did something about it and gave back to the universe.</p>
+                <p>So here I am, writing a post that actually breaks down how to build your own projects so that you can start making stuff.</p>
+                <p>In order to plan and build a programming project there are three key steps I now use when building projects, these are what work for me but they should work for anyone.</p>
+              </div>
+            }
+          />
+          <Post 
+            avatar_sub="./imagines/uwaga.png"
+            subredditName="r/Cykadło"
+            user="u/Rocha2115"
+            time="4"
+            title="How do you get enough protein in your diet to promote muscle growth from your workouts?"
+            mark="Workout"
+            markColor="#8d6a9f"
+            post={
+              <div>
+                <p>I just started tracking macros and... it's depressing. I thought I was doing ok but I'm having way too little protein.</p>
+                <p>I don't really like meat other than red meat, like I can't just sit and eat a piece of chicken. But yesterday I had (among other things): peanut butter on sprouted bread that had protein, cottage cheese, a protein shake, collagen supplement, and chicken sausage for dinner as my main sources of protein and I was way way under the recommended 120 grams. I was at like 78 g. I also had Hummus and an avocado, which I know have a little protein too.</p>
+                <p>I have been doing more bodyweight exercises, but I'm not seeing the results I want and I think it's diet related. What do you all do here to get enough protein to supplement workouts/what would you recommend for me?</p>
+                <p>Edit: I can't reply to everyone, so I'll reply here: thank you all so much!! I have SO many ideas to go off of now. Info I should have included in the post: I'm a woman, and all of my food related goals in the past have been stay skinny so it's been all about cutting calories. This is my first time eating with the goal of being stronger, instead of it being appearance based. I'm so excited to try all of these foods and continue to build strength!</p>
+              </div>
+            }
+          />
         </div>
-        <div className='create' id='trend'>
-          <Button_small text="Best" icon={<BsRocket size={25}/>} />
-          <Button_small text="Hot" icon={<AiOutlineFire size={25}/>} />
-          <Button_small text="New" icon={<FaRegSun size={25}/>} />
-          <Button_small text="Top" icon={<IoPodiumOutline size={25}/>} />
-          <button className='dotdotdot'>
-            <BsThreeDots className='dot'/>
-          </button>
-          <div className='view_dot'>
-          <RxViewHorizontal className='view'/>
-          <RiArrowDownSLine className='view'/>
-          </div>
+        <div className='second_column'>
+
         </div>
-        <Post/>
       </div>
     </>
   );
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,49 +1,104 @@
 import React from 'react';
 import { useState } from 'react';
 import './Post.css';
+
 import { PiArrowFatDownThin } from "react-icons/pi";
 import { PiArrowFatUpThin } from "react-icons/pi";
+import { TfiComment } from "react-icons/tfi";
+import { PiArrowBendUpRightLight } from "react-icons/pi";
+import { CiSaveDown2 } from "react-icons/ci";
+import { BsThreeDots } from "react-icons/bs";
 
+function Post({ subredditName, user, time, title, mark, post, avatar_sub, markColor }) {
+    const [count, setCount] = useState(0);
+    const [upClicked, setUpClicked] = useState(false);
+    const [downClicked, setDownClicked] = useState(false);
 
-function Post() {
-  const [count, setCount] = useState(0);
-  const increment = () => {
-    setCount(count + 1);
-  };
-  const decrement = () => {
-    setCount(count - 1);
-  };
+    const handleUpClick = () => {
+        if (!upClicked && !downClicked) {
+            setCount(count + 1);
+            setUpClicked(true);
+        } else if (upClicked && !downClicked) {
+            setCount(count - 1);
+            setUpClicked(false);
+            setDownClicked(false); 
+        } else if (!upClicked && downClicked) {
+            setCount(count + 2);
+            setUpClicked(true);
+            setDownClicked(false);
+        }
+    };
+
+    const handleDownClick = () => {
+        if (!downClicked && !upClicked) {
+            setCount(count - 1);
+            setDownClicked(true);
+        } else if (!upClicked && downClicked) {
+            setCount(count + 1); 
+            setDownClicked(false);
+        } else if (upClicked && !downClicked) {
+            setCount(count - 2);
+            setUpClicked(false);
+            setDownClicked(true);
+        }
+    };
+
   return (
     <>
         <div className='post'>
             <div className='column_1'>
                 <div className='arrows_counter'>
-                    < PiArrowFatUpThin className='arrow' onClick={increment}/>
+                    <PiArrowFatUpThin className={`arrow ${upClicked ? 'clicked' : ''}`} onClick={handleUpClick} />
                     <div className='counter_p'>
                         <div className='counter'>
                             {count}
                         </div>
                     </div>
-                    <PiArrowFatDownThin className='arrow' onClick={decrement}/>
+                    <PiArrowFatDownThin className={`arrow ${downClicked ? 'clicked down' : ''}`} onClick={handleDownClick} />
                 </div>
-                
             </div>
             <div className='column_2'>
                 <div className='title'>
-                    <img src="./imagines/uwaga.png" alt="" className='ravatar'/>
+                    <img src={avatar_sub} alt="" className='ravatar'/>
                     <div className='rplace'>
-                        r/Cykadło
+                        {subredditName}
                     </div>
                     <div className='kropa'>
-                    •
+                        •
                     </div>
                     <div className='user_post'>
-                    Posted by u/Rocha2115 
+                        Posted by {user} 
                     </div>
                     <div className='time_post'>
-                        5 hours ago   
+                        {time} hours ago   
                     </div>
-
+                </div>
+                <div className='content'>
+                    <div className='title_post'>
+                        {title}
+                        <div className='title_mark' style={{background: markColor}}>
+                            {mark}
+                        </div>
+                    </div>
+                    <div className='text_post'>
+                        {post}
+                    </div>
+                </div>
+                <div className='gradient'></div>
+                <div className='action_buttons'>
+                    <div className='action_buttons_box'>
+                        <TfiComment className='button' id='com'/>
+                        2 Comments
+                    </div>
+                    <div className='action_buttons_box'>
+                        <PiArrowBendUpRightLight className='button' id='share' />
+                        Share
+                    </div>              
+                    <div className='action_buttons_box'>
+                        <CiSaveDown2 className='button' id='save'/>
+                        Save
+                    </div>
+                    <BsThreeDots className='dot'/>
                 </div>
             </div>
         </div>
